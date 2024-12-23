02e46cf134e2429d1182c2c9bbe155b2d0a05b14
Izanmg <128357083+Izanmg@users.noreply.github.com>
2024-12-23 12:04:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function redirectToLanguagePage() {
     const lang = document.getElementById('language').value;
     if (lang === 'es') {
-        window.location.href = 'index-es.html'; // Cambia a la URL de tu página en español
+        window.location.href = 'index.html'; // Cambia a la URL de tu página en español
     } else if (lang === 'en') {
         window.location.href = 'index-en.html'; // Cambia a la URL de tu página en inglés
     }
