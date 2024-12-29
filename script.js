828be45e1291299f753359d15c2e6751240ac37f
Izanmg <128357083+Izanmg@users.noreply.github.com>
2024-12-29 15:27:28 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function redirectToLanguagePage() {
-    const lang = document.getElementById('language').value;
-    if (lang === 'es') {
-        window.location.href = 'index.html'; // Cambia a la URL de tu página en español
-    } else if (lang === 'en') {
-        window.location.href = 'index-en.html'; // Cambia a la URL de tu página en inglés
-    }
-}
