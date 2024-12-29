4f8f2140ad579ffdac2ab147c47c14c4ea8a7887
Izanmg <128357083+Izanmg@users.noreply.github.com>
2024-12-29 15:27:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function redirectToLanguagePage() {
+    const lang = document.getElementById('language').value;
+    if (lang === 'es') {
+        window.location.href = 'index.html'; // Cambia a la URL de tu página en español
+    } else if (lang === 'en') {
+        window.location.href = 'index-en.html'; // Cambia a la URL de tu página en inglés
+    }
+}
